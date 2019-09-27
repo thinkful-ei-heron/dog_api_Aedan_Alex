435959b82ace496e01885ae34d42b55319cfe746
Alex Bannow <rbannal@gmail.com>
2019-09-27 13:26:01 -0400

    added breed search and css

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 function renderDogs(str) {
-    $('#js-dogs').html(str);
+  $('#js-dogs').html(str);
 }
 
 function getDogs(numDogs) {
-    fetch(`https://dog.ceo/api/breeds/image/random/${numDogs}`)
+  fetch(`https://dog.ceo/api/breeds/image/random/${numDogs}`)
     .then(response => response.json())
     .then(jsonString => getDogHtml(jsonString.message))
     .catch(e => console.log(e));
@@ -12,28 +12,61 @@ function getDogs(numDogs) {
 
 function getDogHtml(arrayDog) {
     
-    let htmlString = '';
-    for(let i = 0; i < arrayDog.length; i++) {
-        console.log(arrayDog[i]);
-        htmlString += `<img src="${arrayDog[i]}" alt="Random image of dog"></img>`;
-    }
-    renderDogs(htmlString);
+  let htmlString = '';
+  for(let i = 0; i < arrayDog.length; i++) {
+    console.log(arrayDog[i]);
+    htmlString += `<img src="${arrayDog[i]}" class="dog-gallery" alt="Random image of dog"></img>`;
+  }
+  renderDogs(htmlString);
 }
 
 function handleFormSubmit() {
-    $('#js-user-form').on('submit', e => {
-        e.preventDefault();
-        let input = $('#js-input').val();
-        if (input <= 0) {
-            alert('Too few dogs.');
-        } else if (input > 50) {
-            alert('Too many dogs');
-        } else getDogs(input);
-    });
+  $('#js-user-form').on('submit', e => {
+    e.preventDefault();
+    let input = $('#js-input').val();
+    if (input <= 0) {
+      alert('Too few dogs.');
+    } else if (input > 50) {
+      alert('Too many dogs');
+    } else getDogs(input);
+  });
+}
+
+function renderBreed(string) {
+  $('#breed-pic').html(string);
+}
+
+function getBreedHtml(string, breed) {
+  let imgString = '';
+  console.log(string.message);
+  if(string.status === 'success') {
+    imgString = `<img id="#breedPic" src=${string.message} alt="Random image of ${breed}"></img>`;
+    renderBreed(imgString);
+  }
+  else {
+    throw(string);
+  }
+}
+
+function getBreeds(input){
+  fetch(`https://dog.ceo/api/breed/${input}/images/random`)
+    .then(response => response.json())
+    .then(jsonString => getBreedHtml(jsonString, input))
+    .catch(e => alert(e.message));
+}
+
+function handleBreedSubmit() {
+  $('#js-dog-breed').on('submit', e =>{
+    e.preventDefault();
+    let input = $('#breed-input').val();
+    input.replace(' ', '-');
+    getBreeds(input);
+  });
 }
 
 function main() {
-    handleFormSubmit();
+  handleFormSubmit();
+  handleBreedSubmit();
 }
 
 $(main);
